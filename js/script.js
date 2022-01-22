68ab95769dd8a049de74e83d73c5202f8057f204
Wiktor Gindorowicz <wiktorgindorowicz@gmail.com>
2022-01-22 22:01:32 +0100

    Zmiany modułowe 2

--- js/script.js
+++ js/script.js
@@ -7,3 +7,24 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+var computerMove, playerMove;
+
+
+'! Jeśli Twój ruch to papier, to wygrywasz!';
+var computerMove = 'kamień';
+var playerMove = 'papier';
+playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to przegrywasz!');
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
